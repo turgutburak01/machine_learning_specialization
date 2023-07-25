7b46254fd444aa84c83fdce420b4c29ecac19c51
turgutburak01 <turgutburak0144@gmail.com>
2023-07-25 08:30:46 +0300

    Create my_utils-checkpoint.py

---
+++ coursera/.ipynb_checkpoints/my_utils-checkpoint.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def load_data():
+    data = np.loadtxt('data/ex1data1.txt', delimiter=',')
+    X = data[:,0]
+    y = data[:,1]
+    return X,y
+    
+def load_data_multi():
+    data = np.loadtxt('data/ex1data2.txt', delimiter=',')
+    X = data[:,0]
+    y = data[:,1]
+    return X,y    
